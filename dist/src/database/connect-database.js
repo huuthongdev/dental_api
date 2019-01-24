17f04e53bd0d8a0b387267a1f51e0f122dedc3dd
huuthongdev <huuthong.mgd@gmail.com>
2019-01-24 23:19:12 +0700

    deploy

--- dist/src/database/connect-database.js
+++ dist/src/database/connect-database.js
@@ -7,7 +7,7 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const refs_1 = require("../refs");
 mongoose_1.default.Promise = global.Promise;
 function connectDatabase() {
-    return mongoose_1.default.connect(refs_1.DATABASE_URI, { promiseLibrary: global.Promise })
+    return mongoose_1.default.connect(refs_1.DATABASE_URI, { useMongoClient: true, promiseLibrary: global.Promise })
         .then(() => console.log(`DATABASE CONNECTED SUCCESSFULLY TO ${refs_1.DATABASE_URI}.`))
         .catch(error => (console.log(error), process.exit(1)));
 }
